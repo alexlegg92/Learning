98943a7119668aad3bd62519d4eb80ec634177b1
alexlegg92 <alex.legg7@proton.me>
2023-09-17 21:35:01 +0100

    Added comments.

--- Learning/src/Sum.java
+++ Learning/src/Sum.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 /** User inputs two numbers (variables A and B). Add them together and present the sum. Use methods. */
 
 public class Sum {
+    // I want to return a String.
     static String returnMessageAndSum(int a, int b) {
         return "The sum is " + (a + b);
     }
 
     public static void main(String[] args) {
+        // Use Scanner class to get user input. And create an object of the class.
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter first number:");
+        // We can call methods on the Scanner object.
         int a = scanner.nextInt();
         System.out.println("Enter second number:");
         int b = scanner.nextInt();
