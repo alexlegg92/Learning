a78c2771cb658b700bdb00c28c43b2e57681a310
alexlegg92 <alex.legg7@proton.me>
2023-09-22 05:56:57 +0100

    Bike class: containing objects, instances of the Bicycle class.

--- Learning/src/Bike.java
+++ Learning/src/Bike.java
@@ -1,4 +1,22 @@
 public class Bike {
     public static void main(String[] args) {
+        // Two different bicycle objects
+        Bicycle cervelo = new Bicycle();
+        Bicycle carrera = new Bicycle();
+
+        cervelo.setTopSpeed(10);
+        cervelo.frame("Carbon");
+        cervelo.printState();
+
+        carrera.setTopSpeed(2);
+        carrera.frame("Aluminium");
+        carrera.printState();
+
+        carrera.setTopSpeed(4);
+        carrera.frame("Carbon");
+        carrera.printState();
+
+        // The Bicycle object can contain the Metals object ...
+        //System.out.println(carrera.metal.rigidity("Carbon"));
     }
 }
