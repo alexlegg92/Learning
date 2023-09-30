a6ca10a9cd0317dfbfae97c254b0410e62e59317
alexlegg92 <alex.legg7@proton.me>
2023-09-30 20:13:26 +0100

    A Spring Boot project.

---
+++ JobConnectAgency/src/main/java/com/website/JobConnectAgency/JobConnectAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.website.JobConnectAgency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobConnectAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobConnectAgencyApplication.class, args);
+	}
+
+}
---
+++ JobConnectAgency/src/test/java/com/website/JobConnectAgency/JobConnectAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.JobConnectAgency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobConnectAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
